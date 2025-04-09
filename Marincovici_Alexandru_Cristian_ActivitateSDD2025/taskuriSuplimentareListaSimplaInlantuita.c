c33d950aabddd8665d83d0c91db19486742ad702
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-09 14:01:04 +0300

    Funcție de inițializare

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/taskuriSuplimentareListaSimplaInlantuita.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/taskuriSuplimentareListaSimplaInlantuita.c
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,3 +12,16 @@ typedef struct Magazin {
 	float pretP;
 
 } Magazin;
+
+// Funcție de inițializare
+Magazin initializareMagazin(unsigned int cod, const char* denumire, int nrProduse, float pretP) {
+
+	Magazin m;
+	m.cod = cod;
+	m.denumire = (char*)malloc(strlen(denumire) + 1);
+	strcpy(m.denumire, denumire);
+	m.nrProduse = nrProduse;
+	m.pretP = pretP;
+	return m;
+
+}
