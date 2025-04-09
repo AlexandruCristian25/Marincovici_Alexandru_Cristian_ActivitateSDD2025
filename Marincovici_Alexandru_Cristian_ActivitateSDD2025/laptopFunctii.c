41ac47b5c8a87de82f08e18613f5b7a73c049996
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-09 12:29:44 +0300

    Intializare laptop

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/laptopFunctii.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/laptopFunctii.c
@@ -13,3 +13,16 @@ typedef struct Laptop {
 	float pretL;
 
 }Laptop;
+
+//Intializare laptop
+Laptop initializareLaptop(unsigned int id, const char* producator,
+	int RAM, float pretL) {
+
+	Laptop l;
+	l.id = id;
+	l.producator = (char*)malloc(sizeof(char) * strlen(producator) + 1);
+	strcpy(l.producator, producator);
+	l.RAM = RAM;
+	l.pretL = pretL;
+
+}
