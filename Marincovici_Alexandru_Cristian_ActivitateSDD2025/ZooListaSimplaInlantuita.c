2453a5777dbde1e98be84b82a10efbde7243a06e
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-11 14:24:09 +0300

    Functie de citire a unui Zoo din fisier

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/ZooListaSimplaInlantuita.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/ZooListaSimplaInlantuita.c
@@ -21,3 +21,56 @@ typedef struct Nod {
 	struct Nod* next;
 
 }Nod;
+
+// Functie de citire a unui Zoo din fisier
+Zoo citireZooDinFisier(FILE* f) {
+
+	char buffer[100];
+	Zoo z1;
+
+	if (fgets(buffer, sizeof(buffer), f)) {
+
+		char* token;
+		const char* sep = ",\n";
+		token = strtok(buffer, sep);
+
+		if (token) {
+
+			z1.id = atoi(token);
+
+		}
+		token = strtok(NULL, sep);
+
+		if (token) {
+
+			z1.nume = (char*)malloc(strlen(token) + 1);
+			strcpy(z1.nume, token);
+
+		}
+		token = strtok(NULL, sep);
+
+		if (token) {
+
+			z1.nrAnimale = atoi(token);
+
+		}
+		token = strtok(NULL, sep);
+
+		if (token) {
+
+			z1.nrAngajati = atoi(token);
+
+		}
+		token = strtok(NULL, sep);
+
+		if (token) {
+
+			z1.pretBilet = atof(token);
+
+		}
+		token = strtok(NULL, sep);
+
+	}
+
+	return z1;
+}
