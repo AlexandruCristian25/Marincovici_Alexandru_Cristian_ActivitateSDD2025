6f58f5657db0e5fdd48722ffc7d20af7e749b96a
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-12 11:45:55 +0300

    Creare structura Magazin

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/taskuriSuplimentareListaDublaInlantuita.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Creare structura Magazin
+typedef struct Magazin {
+
+	unsigned int cod;
+	char* denumire;
+	int nrProduse;
+	float pretP;
+
+} Magazin;
