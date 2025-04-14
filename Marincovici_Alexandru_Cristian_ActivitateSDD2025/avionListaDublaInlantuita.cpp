f92353a8e8c931f47e440ea1d2c8c3f4d6e95fce
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-14 14:58:14 +0300

    Creare structura Avion

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/avionListaDublaInlantuita.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
+// Creare structura Avion
+typedef struct Avion {
+
+	unsigned int id;
+	char* nume;
+	int nrPasageri;
+	float pret;
+
+}Avion;
