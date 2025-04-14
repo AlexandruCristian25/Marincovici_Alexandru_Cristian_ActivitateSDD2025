e0c7834fe2680fc859d2eb44f73dc7777e0373df
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-14 14:54:17 +0300

    Functia de initializare

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/jocListaDublaInlantuita.cpp
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/jocListaDublaInlantuita.cpp
@@ -13,3 +13,27 @@ typedef struct Joc {
 	char* producator;
 
 }Joc;
+
+// Functia de initializare
+Joc initializareJoc(unsigned int id, const char* nume,
+	int nrCopii, const char producator) {
+
+	Joc j;
+	j.id = id;
+	j.nume = (char*)malloc(sizeof(char) * strlen(nume) + 1);
+	strcpy(j.nume, nume);
+	j.nrCopii = nrCopii;
+	j.producator = (char*)malloc(sizeof(char) * strlen(producator) + 1);
+	strcpy(j.producator, producator);
+
+	return j;
+
+}
+
+typedef struct Nod {
+
+	Joc joc;
+	struct Nod* next;
+	struct Nod* prev;
+
+}Nod;
