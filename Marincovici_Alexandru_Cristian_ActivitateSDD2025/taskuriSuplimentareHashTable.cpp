7f654a52909b66837c401c9b3d23cfa6715df300
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-16 15:36:01 +0300

    Creare structura Cladire

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/taskuriSuplimentareHashTable.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+// Creare structura Cladire
+typedef struct Cladire {
+
+	unsigned int id;
+	char* adresa;
+	int anConstruire;
+	int nrApartamente;
+
+}Cladire;
