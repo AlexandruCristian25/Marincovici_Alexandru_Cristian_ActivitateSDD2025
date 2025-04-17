b13c8ca3f9d9899808268ca593ccd91740fdc611
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-17 22:13:25 +0300

    Initializare tabela de dispersie

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/magazinHashTable.cpp
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/magazinHashTable.cpp
@@ -99,3 +99,19 @@ void adaugaMagazinInLista(Nod* cap, Magazin magazinNou) {
 	p->next = nou;
 
 }
+
+// Initializare tabela de dispersie
+HashTable initializareHashTable(int dimensiune) {
+
+	HashTable ht;
+	ht.dim = dimensiune;
+	ht.tabela = (Nod**)malloc(dimensiune * sizeof(Nod*));
+	for (int i = 0; i < dimensiune; i++) {
+
+		ht.tabela[i] = NULL;
+
+	}
+
+	return ht;
+
+}
