e7fd4a140430d361ebd31122bbd80f5fbe465ab1
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-18 17:06:23 +0300

    Initializare tabela de dispersie

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/smartwatchHashTable.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/smartwatchHashTable.c
@@ -115,3 +115,19 @@ void adaugaInLista(Nod* cap, Smartwatch smartwatchNou) {
 	p->next = nou;
 
 }
+
+// Initializare tabela de dispersie
+HashTable initializareHashTable(int dimensiune) {
+
+	HashTable ht;
+	ht.dim = dimensiune;
+	ht.tabela = (Nod**)malloc(dimensiune * sizeof(Nod*));
+	for (int i = 0; i < dimensiune; i++) {
+
+		ht.tabela[i] = NULL;
+
+	}
+
+	return ht;
+
+}
