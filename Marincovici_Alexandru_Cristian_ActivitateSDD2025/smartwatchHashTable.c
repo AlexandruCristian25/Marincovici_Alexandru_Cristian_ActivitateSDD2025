87138de9a932fc65d15891da99e405460baaf8b8
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-18 17:06:50 +0300

    Functia de calculare a hash-ului

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/smartwatchHashTable.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/smartwatchHashTable.c
@@ -131,3 +131,17 @@ HashTable initializareHashTable(int dimensiune) {
 	return ht;
 
 }
+
+// Functia de calculare a hash-ului
+int calculeazaHash(const char* nume, int dimensiune) {
+
+	int suma = 0;
+	for (int i = 0; i < strlen(nume); i++) {
+
+		suma += nume[i];
+
+	}
+
+	return suma % dimensiune;
+
+}
