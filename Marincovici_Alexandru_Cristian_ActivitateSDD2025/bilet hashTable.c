2a33cf984f471e386109812b786d5b469f9fd9de
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-18 21:51:54 +0300

    Creare structura Bilet

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/bilet hashTable.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
+// Creare structura Bilet
+typedef struct Bilet {
+	unsigned int id;
+	char* nume;
+	int nrLoc;
+	float pret;
+} Bilet;
