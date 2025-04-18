3eb8967070bffa7666e401bd081b3b1149c16f8a
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-18 17:07:31 +0300

    Functia de afisare a tabelei

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/smartwatchHashTable.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/smartwatchHashTable.c
@@ -192,3 +192,24 @@ HashTable citireDinFisier(const char* numeFisier, int dimensiune) {
 	return hash;
 
 }
+
+// Functia de afisare a tabelei
+void afisareTabela(HashTable ht) {
+
+	for (int i = 0; i < ht.dim; i++) {
+
+		if (ht.tabela[i] != NULL) {
+
+			printf("\nSmartwatch-urile de pe pozitia %d sunt:\n", i);
+			afisareLista(ht.tabela[i]);
+
+		}
+		else {
+
+			printf("\nPe pozitia %d nu avem magazine\n", i);
+
+		}
+
+	}
+
+}
