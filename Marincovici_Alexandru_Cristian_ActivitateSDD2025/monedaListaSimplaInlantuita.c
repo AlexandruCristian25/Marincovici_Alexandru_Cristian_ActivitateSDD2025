912b084f7753a19d6d6d66c069eb269ddf525e6c
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-21 20:46:38 +0300

    Citire din fisier

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/monedaListaSimplaInlantuita.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/monedaListaSimplaInlantuita.c
@@ -20,3 +20,33 @@ typedef struct Nod {
 	struct Nod* next;
 
 }Nod;
+
+// Citire din fisier
+Moneda citireMonedaFisier(FILE* f) {
+
+	char buffer[100];
+	Moneda m = { NULL, 0, 0.0f, 0 };
+
+	if (fgets(buffer, sizeof(buffer), f)) {
+
+		char* token = strtok(buffer, ",;\n");
+		if (token == NULL) return m;
+		m.taraEmitenta = (char*)malloc(strlen(token) + 1);
+		strcpy(m.taraEmitenta, token);
+
+		token = strtok(NULL, ",;\n");
+		if (token == NULL) return m;
+		m.valoareN = atoi(token);
+
+		token = strtok(NULL, ",;\n");
+		if (token == NULL) return m;
+		m.greutate = (float)atof(token);
+
+		token = strtok(NULL, ",;\n");
+		if (token == NULL) return m;
+		m.anEmitere = atoi(token);
+
+	}
+
+	return m;
+}
