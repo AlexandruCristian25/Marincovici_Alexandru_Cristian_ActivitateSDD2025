6867943f4140d36d5d56d040520fc28071eb9a4b
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-24 12:02:52 +0300

    Creare structura Fisier

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/fisierHashTable.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+// Creare structura Fisier
+typedef struct Fisier {
+
+	char* denumire;
+	char* caleF;
+	char* extensieF;
+	unsigned int dimensiune;
+
+}Fisier;
