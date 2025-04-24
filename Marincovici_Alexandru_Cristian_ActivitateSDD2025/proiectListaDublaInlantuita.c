ea62fb5d999417c23186ec31446467a458dc830c
Alexandru Cristian <Alexandru Cristian@DESKTOP-OGCJLS1>
2025-04-24 12:11:52 +0300

    Functia de adaugare a unui element la finalul listei

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/proiectListaDublaInlantuita.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/proiectListaDublaInlantuita.c
@@ -140,3 +140,27 @@ void afisareListaSfarsit(ListaDubla lista) {
 	}
 
 }
+
+// Functia de adaugare a unui element la finalul listei
+void adaugaLaFinalInLista(ListaDubla* lista, Proiect proiectNou) {
+
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->proiect = proiectNou;
+	nou->next = NULL;
+	nou->prev = lista->last;
+
+	if (lista->last != NULL) {
+
+		lista->last->next = nou;
+
+	}
+	else {
+
+		lista->first = nou;
+
+	}
+
+	lista->last = nou;
+	lista->nrNoduri++;
+
+}
