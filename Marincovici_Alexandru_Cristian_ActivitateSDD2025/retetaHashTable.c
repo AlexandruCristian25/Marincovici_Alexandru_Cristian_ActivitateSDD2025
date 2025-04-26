7f2d26a63cd169022852e8f54303b2662cc1ed42
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-04-26 19:54:11 +0300

    Citire din fisier

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/retetaHashTable.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/retetaHashTable.c
@@ -29,3 +29,50 @@ typedef struct HashTable {
 	Nod** tabela;
 
 }HashTable;
+
+//Citire din fisier
+Reteta citireFisier(FILE* f) {
+
+	char buffer[100];
+	Reteta r = { 0, 0, NULL, 0.0f };
+
+	if (fgets(buffer, sizeof(buffer), f)) {
+
+		char* token = strtok(buffer, ",;\n");
+		if (token == NULL) {
+
+			return r;
+
+		}
+		r.nr = atoi(token);
+
+		token = strtok(NULL, ",;\n");
+		if (token == NULL) {
+
+			return r;
+
+		}
+		r.nrMedicamente = atoi(token);
+
+		token = strtok(NULL, ",;\n");
+		if (token == NULL) {
+
+			return r;
+
+		}
+		r.numeMedic = (char*)malloc(sizeof(char) * strlen(token) + 1);
+		strcpy(r.numeMedic, token);
+
+		token = strtok(NULL, ",;\n");
+		if (token == NULL) {
+
+			return r;
+
+		}
+		r.valoare = (float)atof(token);
+
+	}
+
+	return r;
+
+}
