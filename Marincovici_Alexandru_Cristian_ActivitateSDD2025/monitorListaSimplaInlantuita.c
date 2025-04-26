06645d906b215426c0dd58439a65efb53c3bf6a7
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-04-26 20:15:15 +0300

    Functia de initializare

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/monitorListaSimplaInlantuita.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/monitorListaSimplaInlantuita.c
@@ -13,3 +13,18 @@ typedef struct Monitor {
 	int nrPorturi;
 
 }Monitor;
+
+// Functia de initializare
+Monitor initializareMonitor(unsigned int cod, char* producator,
+	float diagonala, int nrPorturi) {
+
+	Monitor m;
+	m.cod = cod;
+	m.producator = (char*)malloc(sizeof(char) * strlen(producator) + 1);
+	strcpy(m.producator, producator);
+	m.diagonala = diagonala;
+	m.nrPorturi = nrPorturi;
+
+	return m;
+
+}
