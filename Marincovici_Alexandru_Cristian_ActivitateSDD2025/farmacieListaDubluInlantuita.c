c95cd6f1decaf73cd27d65a855fda8a776aeff82
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-04-26 20:06:50 +0300

    Funcția de inițializare

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/farmacieListaDubluInlantuita.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/farmacieListaDubluInlantuita.c
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
@@ -12,3 +12,23 @@ typedef struct Farmacie {
 	int nrAngajati;
 
 }Farmacie;
+
+// Funcția de inițializare
+Farmacie initializareFarmacie(const char* denumire, float suprafata, int nrAngajati) {
+
+	Farmacie fa;
+	fa.denumire = (char*)malloc(strlen(denumire) + 1);
+	strcpy(fa.denumire, denumire);
+	fa.suprafata = suprafata;
+	fa.nrAngajati = nrAngajati;
+	return fa;
+
+}
+
+typedef struct Nod {
+
+	Farmacie farmacie;
+	struct Nod* next;
+	struct Nod* prev;
+
+} Nod;
