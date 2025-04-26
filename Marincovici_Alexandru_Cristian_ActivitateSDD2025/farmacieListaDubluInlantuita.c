88a51e3ee407ab348f85071a5dbcade4e26d37b3
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-04-26 20:04:07 +0300

    Creare structura Farmacie

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/farmacieListaDubluInlantuita.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+// Creare structura Farmacie
+typedef struct Farmacie {
+
+	char* denumire;
+	float suprafata;
+	int nrAngajati;
+
+}Farmacie;
