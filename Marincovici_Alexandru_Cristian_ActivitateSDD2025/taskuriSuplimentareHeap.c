b5b41e84c8f1324ed26edfefc9a06e62d83db838
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-04-29 17:00:33 +0300

    Creare structura Pacient

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/taskuriSuplimentareHeap.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+// Creare structura Pacient
+typedef struct Pacient {
+
+	unsigned int id;
+	char* nume;
+	int varsta;
+	int inaltime;
+	int gradUrgenta;
+
+}Pacient;
