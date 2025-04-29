5c59e5fae9a2496ba1ba82a56745fad8caa6cfb1
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-04-29 17:01:09 +0300

    Citire din fișier

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/taskuriSuplimentareHeap.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/taskuriSuplimentareHeap.c
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
@@ -23,3 +23,38 @@ typedef struct Heap {
 	int nrPacienti;
 
 }Heap;
+
+// Citire din fișier
+Pacient citireDinFisier(FILE* f) {
+
+	char buffer[100];
+	Pacient p = { 0, NULL, 0, 0 };
+
+	if (fgets(buffer, sizeof(buffer), f)) {
+
+		char* token = strtok(buffer, ",;\n");
+		if (token == NULL) return p;
+		p.id = atoi(token);
+
+		token = strtok(NULL, ",;\n");
+		if (token == NULL) return p;
+		p.nume = (char*)malloc(strlen(token) + 1);
+		strcpy(p.nume, token);
+
+		token = strtok(NULL, ",;\n");
+		if (token == NULL) return p;
+		p.varsta = atoi(token);
+
+		token = strtok(NULL, ",;\n");
+		if (token == NULL) return p;
+		p.inaltime = atoi(token);
+
+		token = strtok(NULL, ",;\n");
+		if (!token) return p;
+		p.gradUrgenta = atoi(token);
+
+	}
+
+	return p;
+
+}
