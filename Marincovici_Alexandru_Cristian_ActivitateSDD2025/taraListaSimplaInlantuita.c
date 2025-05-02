bcf231a4cb612f78ded69bce82f26cae197ca3e4
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-02 16:54:55 +0300

    Functia de citire a unei Tara din fisier

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/taraListaSimplaInlantuita.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/taraListaSimplaInlantuita.c
@@ -21,3 +21,50 @@ typedef struct Nod {
 	struct Nod* next;
 
 }Nod;
+
+// Functia de citire a unei Tara din fisier
+Tara citireDinFisier(FILE* f) {
+
+	char buffer[100];
+	Tara t = { 0, NULL, 0, 0 };
+
+	if (fgets(buffer, sizeof(buffer), f)) {
+
+		char* token = strtok(buffer, ",;\n");
+		if (token == NULL) {
+
+			return t;
+
+		}
+		t.cod = atoi(token);
+
+		token = strtok(NULL, ",;\n");
+		if (token == NULL) {
+
+			return t;
+
+		}
+		t.nume = (char*)malloc(sizeof(char) * strlen(token) + 1);
+		strcpy(t.nume, token);
+
+		token = strtok(NULL, ",;\n");
+		if (token == NULL) {
+
+			return t;
+
+		}
+		t.nrLocuitori = atoi(token);
+
+		token = strtok(NULL, ",;\n");
+		if (token == NULL) {
+
+			return t;
+
+		}
+		t.PIB = atoi(token);
+
+	}
+
+	return t;
+
+}
