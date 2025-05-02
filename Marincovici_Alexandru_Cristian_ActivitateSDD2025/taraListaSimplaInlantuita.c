46b20148cbeb6714dc7021f51b7b4a15c1286042
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-02 16:54:24 +0300

    Creare structura Tara

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/taraListaSimplaInlantuita.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+#include<string.h>
+
+// Creare structura Tara
+typedef struct Tara {
+
+	unsigned int cod;
+	char* nume;
+	int nrLocuitori;
+	int PIB
+
+}Tara;
