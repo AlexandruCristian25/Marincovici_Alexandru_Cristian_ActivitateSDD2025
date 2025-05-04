12a90e983a21e03f2e601929d9b264ce86e1a40b
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-04 18:52:18 +0300

    Initializare Ghiozdan

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/ghiozdanVectori.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/ghiozdanVectori.c
@@ -13,3 +13,18 @@ typedef struct Ghiozdan {
 	float pret;
 
 }Ghiozdan;
+
+// Initializare Ghiozdan
+Ghiozdan initializareGhiozdan(unsigned int id, const char* producator,
+	int capacitate, float pret) {
+
+	Ghiozdan g;
+	g.id = id;
+	g.producator = (char*)malloc(sizeof(char) * strlen(producator) + 1);
+	strcpy(g.producator, producator);
+	g.capacitate = capacitate;
+	g.pret = pret;
+
+	return g;
+
+}
