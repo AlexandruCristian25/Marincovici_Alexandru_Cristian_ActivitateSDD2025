c240445372de130508d68092ec54e41260dfd392
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-05 19:47:07 +0300

    Functia de dezlocare a tabelei

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/dulciuriHashTable.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/dulciuriHashTable.c
@@ -239,3 +239,18 @@ void dezalocareLista(Nod** cap) {
 	*cap = NULL;
 
 }
+
+// Functia de dezlocare a tabelei
+void dezalocareTabela(HashTable* ht) {
+
+	for (int i = 0; i < ht->dim; i++) {
+
+		dezalocareLista(&(ht->tabela[i]));
+
+	}
+
+	free(ht->tabela);
+	ht->tabela = NULL;
+	ht->dim = 0;
+
+}
