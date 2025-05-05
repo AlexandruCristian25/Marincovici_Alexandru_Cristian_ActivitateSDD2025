e0e5431e3a4726c1f1d462959fc99bdd8301e84a
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-05 19:34:57 +0300

    Functia de afisare a tabelei

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/hainaHashTable.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/hainaHashTable.c
@@ -181,3 +181,24 @@ HashTable citireDinFisier(const char* numeFisier, int dimensiune) {
 	return hash;
 
 }
+
+// Functia de afisare a tabelei
+void afisareTabela(HashTable ht) {
+
+	for (int i = 0; i < ht.dim; i++) {
+
+		if (ht.tabela[i] != NULL) {
+
+			printf("Hainele de pe pozitia %d sunt: \n", i);
+			afisareLista(ht.tabela[i]);
+
+		}
+		else {
+
+			printf("\nPe pozitia %d nu avem haine: \n", i);
+
+		}
+
+	}
+
+}
