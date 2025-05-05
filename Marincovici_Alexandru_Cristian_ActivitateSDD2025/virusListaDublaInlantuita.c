3f087df9bf560c06aaf375e135fd79ea74501a4e
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-05 19:37:47 +0300

    Functia de initializare

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/virusListaDublaInlantuita.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/virusListaDublaInlantuita.c
@@ -13,3 +13,19 @@ typedef struct Virus {
 	int nrTulpini;
 
 }Virus;
+
+// Functia de initializare
+Virus initializareVirus(unsigned int cod, const char* nume,
+	const char* tip, int nrTulpini) {
+
+	Virus v;
+	v.cod = cod;
+	v.nume = (char*)malloc(sizeof(char) * strlen(nume) + 1);
+	strcpy(v.nume, nume);
+	v.tip = (char*)malloc(sizeof(char) * strlen(tip) + 1);
+	strcpy(v.tip, tip);
+	v.nrTulpini = nrTulpini;
+
+	return v;
+
+}
