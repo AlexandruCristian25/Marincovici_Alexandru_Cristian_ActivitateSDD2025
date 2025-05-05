9bb117786e5dd090c314394dd291a134717d273d
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-05 19:31:20 +0300

    Creare structura Haina

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/hainaHashTable.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+#include<string.h>
+
+// Creare structura Haina
+typedef struct Haina {
+
+	char* nume;
+	char* culoare;
+	float pret;
+
+}Haina;
