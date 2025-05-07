39abd94ed6a978a0fefe61f913358963757f56a0
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-07 19:45:39 +0300

    Creare struct Haina

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/hainaListaSimplaInlantuita.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+// Creare struct Haina
+typedef struct Haina {
+
+	unsigned int cod;
+	char* producator;
+	int nrExemplare;
+	float pret;
+
+}Haina;
