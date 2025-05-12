c657be8a40f9302145d886dac0ef0a744d762f27
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-12 22:05:01 +0300

    Afisare in main

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/tranzactieHashTable.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/tranzactieHashTable.c
@@ -247,3 +247,19 @@ void dezalocareTabela(HashTable* ht) {
     ht->dim = 0;
 
 }
+
+// Afisare in main
+int main() {
+
+    HashTable ht = citireDinFisier("tranzactii.txt", 6);
+    afisareTabela(ht);
+
+    printf("\n ==========  Lista tranzactii  ============ \n");
+    afisareTabela(ht);
+
+    dezalocareTabela(&ht);
+
+    printf("\n ==========  Lista tranzactii dupa dezalocare  ============ \n");
+    afisareTabela(ht);
+
+}
