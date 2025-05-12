36b868042534b5b3deef4df4b3c5ba3e679acbff
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-12 22:03:04 +0300

    Initializare HashTable

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/tranzactieHashTable.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/tranzactieHashTable.c
@@ -111,3 +111,20 @@ void adaugaInLista(Nod* cap, Tranzactie tranzactieNoua) {
     p->next = nou;
 
 }
+
+// Initializare HashTable
+HashTable initializareHashTable(int dimensiune) {
+
+    HashTable ht;
+    ht.dim = dimensiune;
+    ht.tabela = (Nod**)malloc(dimensiune * sizeof(Nod*));
+    for (int i = 0; i < dimensiune; i++) {
+
+        ht.tabela[i] = NULL;
+
+    }
+
+
+    return ht;
+
+}
