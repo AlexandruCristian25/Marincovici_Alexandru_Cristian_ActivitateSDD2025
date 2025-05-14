185d25dda2d51f8835406c605b66b68934deb059
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-14 22:29:14 +0300

    Functia de adaugare a unui element in lista

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/masinaListaSimplaInantuita.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/masinaListaSimplaInantuita.c
@@ -89,3 +89,30 @@ void afisareLista(Nod* cap) {
 	}
 
 }
+
+// Functia de adaugare a unui element in lista
+void adaugareInLista(Nod** cap, Masina masinaNoua) {
+
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->info = masinaNoua;
+	nou->next = NULL;
+
+	if (*cap == NULL) {
+
+		*cap = nou;
+
+	}
+	else {
+
+		Nod* p = *cap;
+		while (p->next) {
+
+			p = p->next;
+
+		}
+
+		p->next = nou;
+
+	}
+
+}
