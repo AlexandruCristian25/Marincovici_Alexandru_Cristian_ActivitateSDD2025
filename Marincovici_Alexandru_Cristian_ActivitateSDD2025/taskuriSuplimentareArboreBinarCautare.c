89a3b11760836e0531ab0c41c233ca861a8b3365
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-14 22:15:07 +0300

    Initalizare Magazin

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/taskuriSuplimentareArboreBinarCautare.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/taskuriSuplimentareArboreBinarCautare.c
@@ -22,3 +22,18 @@ typedef struct Nod {
 	struct Nod* dreapta;
 
 }Nod;
+
+// Initalizare Magazin
+Magazin initializareMagazin(unsigned int cod, const char* denumire,
+	int nrProduse, float pretP) {
+
+	Magazin m;
+	m.cod = cod;
+	m.denumire = (char*)malloc(sizeof(char) * strlen(denumire) + 1);
+	strcpy(m.denumire, denumire);
+	m.nrProduse = nrProduse;
+	m.pretP = pretP;
+
+	return m;
+
+}
