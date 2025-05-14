e586f7902ddf005297b550cbe8f5fa689e0fc1f1
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-14 22:23:16 +0300

    Creare structura Masina

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/masinaListaSimplaInantuita.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+// Creare structura Masina
+typedef struct Masina {
+
+	unsigned int id;
+	char* producator;
+	int nrUsi;
+	float pret;
+
+}Masina;
