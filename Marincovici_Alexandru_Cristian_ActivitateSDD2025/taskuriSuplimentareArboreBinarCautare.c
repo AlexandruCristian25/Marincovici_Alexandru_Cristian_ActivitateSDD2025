ebdd027bf1b92541cee7bdbb9f6830b724ec6ff9
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-14 22:14:43 +0300

    Creare structura Magazin

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/taskuriSuplimentareArboreBinarCautare.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+// Creare structura Magazin
+typedef struct Magazin {
+
+	unsigned int cod;
+	char* denumire;
+	int nrProduse;
+	float pretP;
+
+}Magazin;
