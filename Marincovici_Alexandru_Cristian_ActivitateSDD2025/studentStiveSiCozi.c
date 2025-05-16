3dd9987208a4b7dbf25b96d0360e00475bacfa58
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-16 22:44:33 +0300

    Initializare Student

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/studentStiveSiCozi.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/studentStiveSiCozi.c
@@ -21,3 +21,18 @@ typedef struct Nod {
 	struct Nod* next;
 
 }Nod;
+
+// Initializare Student
+Student initializareStudent(int id, const char* nume,
+	int varsta, float medie) {
+
+	Student s;
+	s.id = id;
+	s.nume = (char*)malloc(sizeof(char) * strlen(nume) + 1);
+	strcpy(s.nume, nume);
+	s.varsta = varsta;
+	s.medie = medie;
+
+	return s;
+
+}
