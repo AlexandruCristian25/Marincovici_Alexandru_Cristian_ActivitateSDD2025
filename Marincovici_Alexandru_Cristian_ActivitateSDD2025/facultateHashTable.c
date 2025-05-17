cc7e0d18711997090964e5ef19ca400bc855c639
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-17 23:12:12 +0300

    Creare structura Facultate

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/facultateHashTable.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+// Creare structura Facultate
+typedef struct Facultate {
+
+	unsigned int cod;
+	char* nume;
+	int nrLocuri;
+	int nrCredite;
+
+}Facultate;
