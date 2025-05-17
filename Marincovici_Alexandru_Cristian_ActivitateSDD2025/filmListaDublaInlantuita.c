4988a89661ab19eee2d134adf0592832a51a7010
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-17 23:23:12 +0300

    Functia de citire a elementelor din fisier

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/filmListaDublaInlantuita.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/filmListaDublaInlantuita.c
@@ -176,3 +176,23 @@ void adaugaLaInceputInLista(ListaDubla* lista, Film filmNou) {
 	lista->nrNoduri++;
 
 }
+
+// Functia de citire a elementelor din fisier
+ListaDubla citireLDDinFisier(const char* numeFisier) {
+
+	FILE* f = fopen(numeFisier, "r");
+	ListaDubla lista;
+	lista.first = NULL;
+	lista.last = NULL;
+	lista.nrNoduri = 0;
+
+	while (!feof(f)) {
+
+		adaugaLaFinalInLista(&lista, citireFisier(f));
+
+	}
+
+	fclose(f);
+	return lista;
+
+}
