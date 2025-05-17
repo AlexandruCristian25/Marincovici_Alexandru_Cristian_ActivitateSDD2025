4a6ee6cf570fcd1ac6735b18f62a48793ae43fa2
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-17 23:15:39 +0300

    Functia de dealocare a tabelei

--- Marincovici_Alexandru_Cristian_ActivitateSDD2025/facultateHashTable.c
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/facultateHashTable.c
@@ -234,3 +234,18 @@ void dezalocareLista(Nod** cap) {
 	*cap = NULL;
 
 }
+
+// Functia de dealocare a tabelei
+void dezalocareTabela(HashTable* ht) {
+
+	for (int i = 0; i < ht->dim; i++) {
+
+		dezalocareLista(&(ht->tabela[i]));
+
+	}
+
+	free(ht->tabela);
+	ht->tabela = NULL;
+	ht->dim = 0;
+
+}
