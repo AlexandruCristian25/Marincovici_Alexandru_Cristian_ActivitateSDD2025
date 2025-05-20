2d1c2637d817c745bd2be595dac683d46b80194a
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-20 22:32:47 +0300

    Creare structura Joc

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/jocArboreBinarCautare.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+// Creare structura Joc
+typedef struct Joc {
+
+    int cod;
+    char* nume;
+    float pret;
+
+} Joc;
