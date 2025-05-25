87a93f17201a9972ad22a5f8534d842ad66dd9ff
Marincovici Alexandru Cristian <marincovicialexandru19@stud.ase.ro>
2025-05-25 16:23:12 +0300

    Creare structura Laptop

---
+++ Marincovici_Alexandru_Cristian_ActivitateSDD2025/laptopStructuriHEAP.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+// Creare structura Laptop
+typedef struct Laptop {
+
+	int id;
+	char* producator;
+	int nrBuc;
+	float pret;
+
+}Student;
